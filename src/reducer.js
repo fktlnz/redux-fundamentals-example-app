f81267f1f5c7a78af5551ecf4d045a25d47095c9
masashi morimoto <masashi.morimoto.vd@ambl.co.jp>
2022-03-14 11:41:03 +0900

    filterSlice.jsとtodosSlice.jsをrootReducerへ

--- src/reducer.js
+++ src/reducer.js
@@ -1,65 +1,10 @@
-function nextTodoId(todos) {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
+import filtersSlice from "./features/filters/filtersSlice";
+import todosSlice from "./features/todos/todosSlice";
 
-const initialState = {
-    todos: [{
-        id: 0,
 
-    }],
-    filters: []
-}
-
-export default appReducer = (state=initialState, action) => {
-    switch(action.type) {
-        case "todos/todoAdded": {
-            return {
-                ...state,
-                todos: [
-                    ...state.todos,
-                    {
-                        id: nextTodoId(state.todos),
-                        text: action.payload,
-                        completed: false
-                    }
-                ]
-            }
-        }
-        case 'todos/todoToggled': {
-            return {
-                // Again copy the entire state object
-                ...state,
-                // This time, we need to make a copy of the old todos array
-                todos: state.todos.map(todo => {
-                // If this isn't the todo item we're looking for, leave it alone
-                if (todo.id !== action.payload) {
-                    return todo
-                }
-
-                // We've found the todo that has to change. Return a copy:
-                return {
-                    ...todo,
-                    // Flip the completed flag
-                    completed: !todo.completed
-                }
-                })
-            }
-        }
-        case 'filters/statusFilterChanged': {
-            return {
-                // Copy the whole state
-                ...state,
-                // Overwrite the filters value
-                filters: {
-                // copy the other filter fields
-                ...state.filters,
-                // And replace the status field with the new value
-                status: action.payload
-                }
-            }    
-        }
-        default: 
-            return state
+export default rootReducer = (state={}, action) => {
+    return {
+        todos: todosSlice(state.todos, action),
+        filters: filtersSlice(state.filters, action)
     }
 }
