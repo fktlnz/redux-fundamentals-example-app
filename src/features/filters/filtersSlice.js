764ca362ef22647c13910128259ca2595b944166
masashi morimoto <masashi.morimoto.vd@ambl.co.jp>
2022-03-14 11:35:34 +0900

    reducer.jsをfiltersSlice.js / todosSlice.jsに分ける（reducer内の記載を簡潔に）

---
+++ src/features/filters/filtersSlice.js
@@ -0,0 +1,29 @@
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
+const initialState = {
+    status: 'All',
+    colors: []
+}
+
+export default appReducer = (state=initialState, action) => {
+    switch(action.type) {
+        case 'filters/statusFilterChanged': {
+            return {
+                // Copy the whole state
+                ...state,
+                // Overwrite the filters value
+                filters: {
+                // copy the other filter fields
+                ...state.filters,
+                // And replace the status field with the new value
+                status: action.payload
+                }
+            }    
+        }
+        default: 
+            return state
+    }
+}
